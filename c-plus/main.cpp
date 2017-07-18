54090d19b75b461f8a9bff9c18f1b6f53bf0fe75
sutiwatk <sutiwatk@gmail.com>
2017-07-18 13:10:33 +0700

    Add files via upload

---
+++ c-plus/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int num;
+	cout << "Initialize Loop..." << endl;
+	
+	for(num=1; num<=10;num++)
+	{
+	
+	cout << "Hello! Sutiwat :" << num << endl;
+}
+	 system("pause");
+	return 0;
+}
